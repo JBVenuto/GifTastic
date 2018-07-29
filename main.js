c698a35ff1af015b1d1cb367d84d4498e8ab2835
Joseph Venuto <jbvenuto@gmail.com>
2018-07-29 11:52:17 -0400

    store which button the user clicked to a variable

--- main.js
+++ main.js
@@ -3,6 +3,28 @@ var animals = ["dog", "cat", "bear", "seal", "sloth", "panda"];
 
 $(document).ready(function() {
 
+    //Function show the results of the gif search
+    $(document).on("click", ".animal-btn", function() {
+
+        //Store the user's choice to search for it on giphy
+        var choice = $(this).attr("data-name");
+        console.log(choice);
+
+
+    });
+
+    //Function to add the user's animal to the array
+    $("#add").on("click", function(event) {
+
+        event.preventDefault();
+        //Stores the user's input
+        var userAnimal = $("#input").val().trim();
+        //Pushes the user's input into the array
+        animals.push(userAnimal);
+        //Make a button with the user's input
+        makeButtons();
+    })
+
     //Function to make the buttons appear for the user
     function makeButtons() {
 
@@ -18,7 +40,7 @@ $(document).ready(function() {
             //Add a class to the button
             bt.addClass("animal-btn btn btn-outline-dark my-2 my-sm-0");
             //Add an attribute that will be the term that is searched
-            bt.attr("data", animals[i]);
+            bt.attr("data-name", animals[i]);
             //Add a margin between buttons
             bt.attr("style", "margin-right:2px")
             //Add text to the button
